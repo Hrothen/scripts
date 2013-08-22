960a69720804ee9f193d736ab72ef8ee89b65625
Leif Grele <lgrele@gmail.com>
2013-08-22 14:43:02 -0700

    Some more work on convertPNG, doesn't work yet

--- src/convertPNG.hs
+++ src/convertPNG.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 import qualified Data.Map.Strict as Map
+import Data.Maybe
 import Data.Aeson
 import Data.List(unzip4)
 import Codec.Picture.Png(decodePng)
@@ -10,11 +11,13 @@ import qualified Data.ByteString.Lazy as L
 import System.Environment(getArgs)
 
 
---main = do
---    args <- getArgs
---    rawpng <- readFile (head args)
---    mapping <- readFile ""
---    qfCSV <- return (convertPNGToCSV)
+{-
+Possible command line arguments:
+--
+-}
+
+main = do
+    args <- getArgs
 
 
 type LongPixel = (Int,Int,Int,Int)
@@ -36,13 +39,21 @@ parsePlace commands mapping = undefined
 parseQuery :: [Int] -> (Int -> String) -> Maybe String
 parseQuery commands mapping = undefined
 
-unzipImage4 :: Pixel pixel => Image pixel -> ([Int],[Int],[Int],[Int])
-unzipImage4 = unzip4 . reverse . pixelFold pred []
+unzipImage4 :: L.ByteString -> Maybe ([Int],[Int],[Int],[Int])
+unzipImage4 str = convert $ decodePng str
+    where convert (Left s) = Nothing
+          convert (Right img) = Just (unzip4 img)
+
+
+imageToList :: (Pixel p,ColorConvertible p PixelRGBA16) => 
+                Image p -> [LongPixel]
+imageToList = reverse . pixelFold pred []
     where pred acc _ _ pix = (promoteToTuple pix):acc
 
 
 promoteToTuple :: (ColorConvertible pixel PixelRGBA16) => pixel -> LongPixel
-promoteToTuple p = pixelToTuple $ promotePixel p
+promoteToTuple p = pixelToTuple p' 
+    where p' = (promotePixel p)::PixelRGBA16
 
 
 pixelToTuple :: PixelRGBA16 -> LongPixel
